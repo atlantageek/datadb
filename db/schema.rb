5b4ec3cb6023f1196765013277783702e7bb12ff
Tommie Jones <atlantageek@gmail.com>
2013-02-28 22:12:23 -0500

    Adding navigation.

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130226051545) do
 
-  create_table "bacon", :id => false, :force => true do |t|
-    t.date   "dt"
-    t.string "val", :limit => 10
-  end
-
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
